01c2352ab2a5ef6f847c205c7bf994a35e447b76
be4o <be4o@users.noreply.github.com>
2017-05-19 12:01:54 -0700

    Update database.php
    update database configration data

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'ca-cdbr-azure-central-a.cloudapp.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'schoolsysdb'),
+            'username' => env('DB_USERNAME', 'b7172f53fb2082'),
+            'password' => env('DB_PASSWORD', '1aab134e'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
