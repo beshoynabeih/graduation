09426ac1ced03f7965753e1458b18a0baded3168
Beshoy Nabeih <c-82403016@s-mu.edu.eg>
2017-08-14 01:32:31 +0200

    delete all secure_url
    I did delete all secure_url function from href

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,14 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>@yield('title')</title>
-<link href="{{URL::asset('images/favicon.png')}}" rel="shortcut icon" />
-<link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet" />
-<link href="{{URL::asset('css/flexslider.css')}}" rel="stylesheet" />
-<link href="{{URL::asset('css/style.css" rel="stylesheet')}}" />
-<link rel="stylesheet" href="{{URL::asset('css/font-awesome.min.css')}}">
-<link rel="stylesheet" href="{{URL::asset('css/custom.css')}}">
-<script src="{{URL::asset('js/jquery.min.js')}}"></script>
-<script type="application/x-javascript')}}"> addEventListener("load", function() { setTimeout(hidesecure_urlbar, 0); }, false); function hidesecure_urlbar(){ window.scrollTo(0,1); } </script>
+<link href="images/favicon.png" rel="shortcut icon" />
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/custom.css">
+<script src="js/jquery.min.js"></script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hidesecure_urlbar, 0); }, false); function hidesecure_urlbar(){ window.scrollTo(0,1); } </script>
 <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
 <style>
 @yield('style')
@@ -20,16 +20,16 @@
 </head>
 <body>
 <div class="header">
-  <div class="container"> <a class="navbar-brand" href="{{ secure_url('/') }}"><img id="logo-img" src="images/favicon.png"/></a>
+  <div class="container"> <a class="navbar-brand" href="/"><img id="logo-img" src="images/favicon.png"/></a>
     <div class="menu"> <a class="toggleMenu" href="#"><img src="images/nav_icon.png" alt="" /> </a>
       <ul class="nav" id="nav">
-        <li ><a href="{{ secure_url('/') }}">Home</a></li>
-        <li ><a href="{{ secure_url('/teachers') }}">Teachers</a></li>
-        <li ><a href="{{ secure_url('/tables') }}">Tables</a></li>
-        <li ><a href=" {{ secure_url('/about') }}">About Us</a></li>
-        <li ><a href="{{ secure_url('contact') }}">Contact Us</a></li>
+        <li ><a href="/">Home</a></li>
+        <li ><a href="/teachers">Teachers</a></li>
+        <li ><a href="/tables">Tables</a></li>
+        <li ><a href=" /about">About Us</a></li>
+        <li ><a href="contact">Contact Us</a></li>
         @if (Auth::guest())
-        <li><a href="{{ secure_url('/login') }}">Login</a></li>
+        <li><a href="/login">Login</a></li>
         @else
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -38,27 +38,27 @@
 
             <ul class="dropdown-menu" role="menu">
               @if(Auth::user()->type == 4)
-              <li><a href="{{ secure_url('/admin') }}"></i>Dashboard</a></li>
-              <li><a href="{{ secure_url('/updatetables') }}"></i>Update Tables</a></li>
+              <li><a href="/admin"></i>Dashboard</a></li>
+              <li><a href="/updatetables"></i>Update Tables</a></li>
 
-              <li><a href="{{ secure_url('/addemployee') }}"></i>Add Employee</a></li>
+              <li><a href="/addemployee"></i>Add Employee</a></li>
               @endif
               @if(Auth::user()->type == 3)
-              <li><a href="{{ secure_url('/affairs') }}"></i>Dashboard</a></li>
+              <li><a href="/affairs"></i>Dashboard</a></li>
               @endif
               @if(Auth::user()->type == 2)
-              <li><a href="{{ secure_url('/teacher') }}"></i>Dashboard</a></li>
+              <li><a href="/teacher'"></i>Dashboard</a></li>
               @endif
               @if(Auth::user()->type == 1)
-              <li><a href="{{ secure_url('/parent') }}"></i>Dashboard</a></li>
+              <li><a href="'/parent'"></i>Dashboard</a></li>
               @endif
-                <li><a href="{{ secure_url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                <li><a href="{{/logout')"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
             </ul>
           </li>
         @endif
         <div class="clear"></div>
       </ul>
-      <script type="text/javascript" src="{{URL::asset('js/responsive-nav.js')}}"></script>
+      <script type="text/javascript" src="js/responsive-nav.js"></script>
     </div>
   </div>
 </div>
@@ -75,14 +75,14 @@
 </div>
 <script>
 document.getElementById("logo-img").onmouseover = function(){
-	document.getElementById("logo-img").setAttribute("src", "{{secure_url('images/favicon2.png')}}");
+	document.getElementById("logo-img").setAttribute("src", "images/favicon2.png");
 };
 document.getElementById("logo-img").onmouseout = function(){
-	document.getElementById("logo-img").setAttribute("src", "{{secure_url('images/favicon.png')}}");
+	document.getElementById("logo-img").setAttribute("src", "images/favicon.png");
 };
 </script>
-<script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
-<script src="{{URL::asset('js/jquery.flexslider.js')}}"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.flexslider.js"></script>
 @yield('scripts')
 </body>
 </html>
